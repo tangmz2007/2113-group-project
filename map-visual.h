955ba99ed4322273ae697cb03d940a9fa8f367e9
solitary-crane <xiongmingyufoyi@outlook.com>
2026-03-30 12:52:28 +0800

    Add files via upload

---
+++ map-visual.h
@@ -0,0 +1,10 @@
+#ifndef MAP_VISUAL_H
+#define MAP_VISUAL_H
+
+#include <vector>
+#include <string>
+#include "gamemap.h"
+
+void printMapVisual(Node gameMap[MAX_LAYERS + 2][MAX_PER_LAYER], const int layersizes[MAX_LAYERS + 2]);
+
+#endif
