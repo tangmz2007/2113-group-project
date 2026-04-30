13fd1895324dbc4d34ee2f76f72dd476e3590e7a
hku3652015-debug <hku3652015@gmail.com>
2026-04-30 19:49:39 +0800

    Add files via upload
    Define a player's default state and skill relix system

--- player.h
+++ player.h
@@ -1,69 +1,51 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include "skill.h"
+#include "types.h"
 #include <vector>
-#include "showData.h"
-#include <string>
-using namespace std;
 
-class Player : public Character {
-public:
-    int HP;
-    int maxEnergy;
-    int currentEnergy;
-    int level;
-    int experience;
-    int gold;
-    vector<skill_node> skills;
-	vector<string> inventory; // 存储物品名称的背包
-    
+struct Player {
+    int hp = 100;
+    int maxHp = 100;
+    int exp = 0;
+    int level = 1;
+    int skillPoints = 0;
+    int gold = 60;
+    int maxEnergy = 3;
+    int skillSlots = 6;
+    int potionSlots = 3;
+    std::vector<int> learnedSkills;
+    std::vector<int> potions;
+    std::vector<int> relics;
 
-public:
-    Player();
+    bool phoenixUsed = false;
+    int backpackCount = 0;
+    int grandmaSkipsLeft = 0;
+    bool nextBattleDummy = false;
+    bool pendingForgetAfterDummy = false;
+};
 
-    //energy system;
-    void resetEnergy();
-    int getEnergy();
-    void gainMaxEnergy(int amount);
-    void gainEnergy(int amount);
+struct RunState {
+    bool valid = false;
+    bool gameOver = false;
+    int curLayer = 0;
+    int curIndex = 0;
+    std::string mapId = "default";
+};
 
-    //leveling system
-    void gainExperience(int amount);
-	void levelUp();
-
-    //experience tree
-    void learnSkill(skill_node skill[]);
-
-
-    //gold system
-    void earnGold(int amount)
-    {
-		gold += amount;
-    }
-    void spendGold(int amount)
-    {
-        if (gold >= amount)
-        {
-			gold -= amount;
-        }
-        else
-        {
-            showdata::showCenteredBox("No enough money !!!");
-        }
-    }
-
-    //display
-    void displayStatus() const;
-    void showenergybar()const;
-    
-
-
-
-}
-
-
-
-
+Player makeNewPlayer();
+void showPlayerStatus(const Player& p);
+void gainExp(Player& p, int v);
+bool hasRelic(const Player& p, int id);
+int countRelic(const Player& p, int id);
+int damageRelicBonus(const Player& p);
+int goldRelicBonus(const Player& p);
+int potionHealBonus(const Player& p);
+bool hasSkill(const Player& p, int id);
+void chooseSkillToForget(Player& p, bool allowCancel);
+void chooseSkillToLearn(Player& p, bool allowCancel);
+void unlockSkillsMenu(Player& p);
+void applyRelicPickup(Player& p, int id);
+void addPotionToInventory(Player& p, int id);
 
 #endif
